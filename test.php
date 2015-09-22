05cb0c23947313a4f46bbcd11366746bc7609e8a
zendev <mateusz@zendev.it>
2015-09-22 11:12:04 +0200

    configs

--- test.php
+++ test.php
@@ -1,6 +1,12 @@
 <?php
 
-echo 1;
+require("vendor/autoload.php");
+$config['beanstalkd']['host'] = 'localhost';
+$config['beanstalkd']['port'] = 11300;
+
+$queue =  new Pheanstalk_Pheanstalk($config['beanstalkd']['host'] . ":" . $config['beanstalkd']['port']);
+$queue->watch("mytube");
+
 
 $vendors = scandir('./vendor', 1);
 var_dump($vendors);
